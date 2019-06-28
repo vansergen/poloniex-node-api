03f226cc3f86e0980bd07922574c8a500755651e
vansergen <vansergen@gmail.com>
2019-06-28 10:55:43 -0400

    perf: add Typescript declarations

---
+++ index.d.ts
@@ -0,0 +1,61 @@
+import { EventEmitter } from 'events';
+
+declare module 'poloniex' {
+  export type callback<T> = (error: any, data: T) => void;
+
+  export class PublicClient {
+    constructor(options?: any);
+
+    get(options: any);
+
+    request(options: any);
+
+    cb(method: string, callback: callback<any>, options?: any);
+
+    getTickers(): Promise<any>;
+
+    getVolume(): Promise<any>;
+
+    getOrderBook(options?: any): Promise<any>;
+
+    getTradeHistory(options?: any): Promise<any[]>;
+
+    getChartData(options: any): Promise<any[]>;
+
+    getCurrencies(): Promise<any>;
+
+    getLoanOrders(options: any): Promise<any>;
+  }
+
+  export class AuthenticatedClient {
+    constructor(options: any);
+
+    post(options: any);
+
+    getBalances(): Promise<any>;
+
+    getCompleteBalances(options?: any): Promise<any>;
+
+    getDepositAddresses(): Promise<any>;
+
+    getNewAddress(options: any): Promise<any>;
+
+    getDepositsWithdrawals(options: any): Promise<any>;
+  }
+
+  export class WebsocketClient extends EventEmitter {
+    constructor(options?: any);
+
+    on(event: 'open', eventHandler: () => void): this;
+    on(event: 'close', eventHandler: () => void): this;
+    on(event: 'error', eventHandler: (error: any) => void): this;
+    on(event: 'message', eventHandler: (data: any) => void): this;
+    on(event: 'raw', eventHandler: (data: any) => void): this;
+
+    connect(): void;
+    disconnect(): void;
+
+    subscribe(options: any): void;
+    unsubscribe(options: any): void;
+  }
+}
