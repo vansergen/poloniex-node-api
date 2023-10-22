e5e9a42313e989e5068bf5928850c268cfd1ea2c
Sergey Bakulin <sergey.bakulin.github@gmail.com>
2023-10-22 19:15:35 +0300

    refactor!: remove legacy API

--- src/legacy/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./public.js";
-export * from "./signer.js";
-export * from "./auth.js";
-export * from "./websocket.js";
-export * from "./currencies.js";
-export * from "./currencypairs.js";
